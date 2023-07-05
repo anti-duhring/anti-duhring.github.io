e434f2be0c45d0175c032f9c77e65a11c21adc52
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-07-04 22:36:02 -0300

    fix: whatsapp link

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -37,7 +37,7 @@ const Contact = () => {
                 <MediaItem title="Github" img={GithubImg} link="https://github.com/anti-duhring" />
                 <MediaItem title="Email" img={GmailImg} link="mailto:mateus.limavn@gmail.com" />
                 <MediaItem title="Linkedin" img={LinkedinImg} link="https://linkedin.com/in/mateus-vinicius-lima/" />
-                <MediaItem title="Whatsapp" img={WppImg} link="(81) 988221999" />
+                <MediaItem title="Whatsapp" img={WppImg} link="https://wa.me/5581988221999" />
             </MediaFlexbox>
             </ContainerBlue>
             <AboutMe />
